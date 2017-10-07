874918bed9d1b08aca9234862d20e488056b0e1f
Benjamin <benjamin.probst@hispeed.ch>
2017-10-07 12:15:29 +0200

    Server

--- main/src/Client_Server/Server/Server.java
+++ main/src/Client_Server/Server/Server.java
@@ -1,2 +1,30 @@
+package Client_Server.Server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    public Server(){
+        try {
+            ServerSocket socket = new ServerSocket(22022);
+            System.out.println("Server waiting");
+
+            Socket server = socket.accept();
+
+            ObjectInputStream objInput = new ObjectInputStream(server.getInputStream());
+
+            ObjectOutputStream objOutput = new ObjectOutputStream(server.getOutputStream());
+            // useObject(objInput.readObject());
+
+            objInput.close();
+            objOutput.close();
+        } catch (Exception e){
+
+        }
+    }
+
+
 }
