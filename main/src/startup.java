75804322651c05740ac5a13238a01ce94e6af5bb
fowns88itn <camillo.schweizer@gmail.com>
2017-10-07 12:32:28 +0200

    Structure

--- main/src/startup.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by camillo.schweizer on 30.09.2017.
- */
-public class startup {
-}
---
+++ src/startup.java
@@ -0,0 +1,13 @@
+/**
+ * Created by camillo.schweizer on 30.09.2017.
+ */
+public class startup {
+
+    public static void main(String [ ] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+
+}
