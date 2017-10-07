937790341a66df894bc1a42a818e1680044fb13b
Benjamin <benjamin.probst@hispeed.ch>
2017-10-07 12:35:15 +0200

    Client

--- src/Client_Server/Client/Client.java
+++ src/Client_Server/Client/Client.java
@@ -1,2 +1,28 @@
+package Client_Server.Client;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Client {
+
+    public Client() {
+
+        try {
+            Socket socketConnection = new Socket("localhost", 22022);
+
+            ObjectOutputStream objOutput = new ObjectOutputStream(socketConnection.getOutputStream());
+            ObjectInputStream objInput = new ObjectInputStream(socketConnection.getInputStream());
+
+            // objOutput.writeObject(TODO);
+
+            // TODO = objInput.readObject();
+
+            objInput.close();
+            objOutput.close();
+
+        } catch (Exception e){}
+
+    }
+
 }
